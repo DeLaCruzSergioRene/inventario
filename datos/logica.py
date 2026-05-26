27c5d62edd60982205d611e67a87a9529d184389
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-05-26 10:58:11 -0600

    Add pedir_producto function to manage product quantity

--- datos/logica.py
+++ datos/logica.py
@@ -12,3 +12,13 @@ def listar(tab):
 def borrar(tab, id_reg):
     db.execute(f"DELETE FROM {tab} WHERE id = ?", (id_reg,))
     db.commit()
+
+def pedir_producto(id_prod, cantidad):
+    if cantidad <= 0:
+        return False
+    fila = db.execute("SELECT cant FROM prod WHERE id = ?", (id_prod,)).fetchone()
+    if not fila or fila[0] < cantidad:
+        return False
+    db.execute("UPDATE prod SET cant = ? WHERE id = ?", (fila[0] - cantidad, id_prod))
+    db.commit()
+    return True
